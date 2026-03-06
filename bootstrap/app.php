a32a45ad265ce69b8bad17053799debb41e4603e
Ahmed <ahmedalsirmubarak@hotmail.com>
2026-03-06 20:09:33 +0400

    Enhance proxy trust configuration in middleware and add sessions migration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +12,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Trust all proxies (needed when behind nginx/load balancer on HTTPS hosting)
-        $middleware->trustProxies(at: '*');
+        // Trust all proxies + all forwarded headers (required on Cloudways/Nginx)
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+        );
 
         // CSRF exceptions
         $middleware->validateCsrfTokens(except: [
